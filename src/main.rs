404d497bef7f02bc3e315b0cb305bbdf65415046
Ryo Koike <ryok022366@gmail.com>
2023-10-13 13:37:56 +0900

    done 12.6

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,12 @@ use minigrep::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
-    if let Err(e) = minigrep::run(config) {
-        println!("Application error: {}", e);
+    minigrep::run(config).unwrap_or_else(|err| {
+        eprintln!("Application error: {}", err);
         process::exit(1);
-    }
+    });
 }
